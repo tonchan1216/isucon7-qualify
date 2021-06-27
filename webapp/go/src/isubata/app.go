1a147e81cfb1bed6989c0d12e704673d641ed026
tonchan1216 <cleartone1216@gmail.com>
2021-06-27 21:38:46 +0900

    sleep

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -460,7 +460,7 @@ func fetchUnread(c echo.Context) error {
 		return c.NoContent(http.StatusForbidden)
 	}
 
-	time.Sleep(time.Second)
+	// time.Sleep(time.Second)
 
 	channels, err := queryChannels()
 	if err != nil {
